ba75df243cc36479044c5eaf89c71898f1a874dd
Stuart Huddy <s.w.m.huddy@dundee.ac.uk>
2016-09-15 01:03:26 +0100

    verify checking succeeed, program in working state.

--- source.py
+++ source.py
@@ -2,13 +2,10 @@ import subprocess
 import threading
 import argparse
 import sys
-import os
-import time
+
 
 parser = argparse.ArgumentParser()
-parser.add_argument("threads", help="each thread must have an associated 'passphrase' Dictionary File")
-parser.add_argument("-d", "--dmg", help=".dmg to crack")
-
+parser.add_argument("dmg", help=".dmg to crack")
 args = parser.parse_args()
 
 class myThread(threading.Thread):
@@ -22,69 +19,37 @@ class myThread(threading.Thread):
         print('File opened.')
 
     def run(self):
-        #print('Thread ' + self.threadID + ' is beginning its journey')i
         for line in self.f:
             self.passphrase = line.rstrip()
             try:
-                process = subprocess.call(['hdiutil', 'verify', '-passphrase', self.passphrase, args.dmg],
-                                          stdout=open(os.devnull,'wb') ,stderr=open(os.devnull, 'wb'))
-                if process == 0:
-                    print('password' + self.passphrase)
+                subprocess.call(['hdiutil', 'verify', '-passphrase', self.passphrase, args.dmg]
+                                                       ,stderr = open('results', 'w'))
+                statinfo = os.stat('results')
+                if statinfo.st_size is 118:
+                    print('password found: ' + self.passphrase)
                     input('HOLD THE PHONE!')
-                else:
-                    print('Failed:' + str(self.threadID) + ' ' + self.passphrase)
-
-
-            except subprocess.CalledProcessError:
-                print(self.passphrase)
-                pass
+                    break
+                print('Failed:' + str(self.threadID) + ' ' + self.passphrase)
+            except:
+                print('Password check error:')
+                print(sys.exc_info()[0])
         self.f.close()
 
 count = 0
-#create new threads
 t = []
 try:
+    while(count < 5)
     newThread = myThread(count, 'passphrase' + str(count))
     t.append(newThread)
     count +=1
-    newThread = myThread(count, 'passphrase' + str(count))
-    t.append(newThread)
-    count +=1
-    newThread = myThread(count, 'passphrase' + str(count))
-    t.append(newThread)
-    count +=1
-    newThread = myThread(count, 'passphrase' + str(count))
-    t.append(newThread)
-    count +=1
-    newThread = myThread(count, 'passphrase' + str(count))
-    t.append(newThread)
-    count +=1
-    newThread = myThread(count, 'passphrase' + str(count))
-    t.append(newThread)
-    count +=1
-    newThread = myThread(count, 'passphrase' + str(count))
-    t.append(newThread)
-    count +=1
-    newThread = myThread(count, 'passphrase' + str(count))
-    t.append(newThread)
-
 except:
     print('Fuck, a thread unable to start')
     print(sys.exc_info()[0])
 
-count = 0
-start = time.time()
 for thread in t:
-    print(count)
     thread.start()
-    count +=1
-
 for thread in t:
-    print('HELLO')
     thread.join()
-end = time.time()
-print(end - start)
-
 print("Exiting program")
 
 
