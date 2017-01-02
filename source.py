15e04dbb5e76319dcc61d301daa939e6c6446240
Alfanhui <program.foo@gmail.com>
2017-01-02 20:31:56 +0000

    Fixed bugs, added structure/dynamic status update

--- source.py
+++ source.py
@@ -1,29 +1,41 @@
+from __future__ import print_function
 import threading
 import sys
 import os
 import shlex
 import Queue
-import time
+import time 
+from time import strftime
 from subprocess import Popen, PIPE
 from optparse import OptionParser
 
+#Global Variables
 queue = Queue.Queue()
+printQueue = Queue.Queue()
 t = []
+timeRemaining = 0
+fileCount = 0
 found = False
+
+#Options
 parser = OptionParser()
 parser.add_option("-u", "--update",
                   action="store_false", dest="update", default=True,
                   help="use to update program")
 (options, args) = parser.parse_args()
 
+#Main Thread
 class MyThread(threading.Thread):
-    def __init__(self, queue):
-        threading.Thread.__init__(self)
+    def __init__(self, queue, printQueue):
+        thread = threading.Thread(target=self.run,args=())
         self.queue = queue
-
+	self.printQueue = printQueue
+	thread.daemon = True
+	thread.start()
+    
     def run(self):
         while not self.queue.empty():
-            passphrase = str(self.queue.get())
+    	    passphrase = str(self.queue.get())
             args = shlex.split("hdiutil verify PJM.dmg -passphrase " + passphrase)
             proc = Popen(args, stdout=PIPE, stderr=PIPE)
             out, err = proc.communicate()
@@ -37,80 +49,92 @@ class MyThread(threading.Thread):
                 found = True
                 return
             else:
-                print("Failed:" + passphrase)
-            time.sleep(0.01)
+		result = str(self.queue.qsize()) + "\tFailed\t" + passphrase 
+		self.printQueue.put(result)
 
     def stop(self):
         self.stopped = True
 
-def main(success):
-    if success:
-        print("closing threads... ")
-        for thread in t:
-            if thread.isAlive():
-                thread.stop()
-        print("Exiting Program.")
-        return
-    else:
-        count = 348
-        fileCheck = False
-        for i in range(count):
-            if os.path.isfile('passphrase' + str(i)):
-                f = open("passphrase" + str(i))
-                for line in f:
-                    check = line.rstrip()
-                    if "FILECOMPLETED" in check:
-                        fileCheck = True
-                        break
-                    else:
-                        count = i
-                        break
-                if fileCheck:
-                    fileCheck = False
-                    continue
-                else:
-                    break
-        if(count !=349):
-            try:
-                for i in range(5):
-                    myThread = MyThread(queue)
-                    myThread.setDaemon(True)
-                    t.append(myThread)
-                f = open("passphrase" + str(count), 'r')
-                for line in f:
-                    queue.put(line.rstrip())
-                f.close()
-                for myThread in t:
-                    myThread.start()
-                    time.sleep(.001)
-                while True:
-                    time.sleep(.001)
-                    if found == True:
-                        raise KeyboardInterrupt
-                queue.join()
-            except KeyboardInterrupt:
-                print("closing threads... ")
-                for thread in t:
-                    if thread.isAlive():
-                        thread.stop()
-                print("Exiting Program.")
-                return
-            except:
-                print("Fuck, a thread unable to start")
-                print(sys.exc_info()[0])
-        else:
-            print("No more files available.")
-        if(queue.empty()):
-            with open("passphrase" + str(count), 'r') as original: data = original.read()
-            with open('passphrase' + str(count), 'w') as modified: modified.write("FILECOMPLETED\n" + data)
+#Time remaining thread
+class timerThread(threading.Thread):
+    def __init__(self,queue,printQueue):
+        thread = threading.Thread(target=self.run,args=())
+        self.queue = queue
+	self.printQueue = printQueue
+	thread.daemon = True
+	thread.start()
+    
+    def run(self):
+	global timeRemaining
+	time.sleep(1)
+	while True:
+	    start = queue.qsize()
+	    time.sleep(1)
+	    end = queue.qsize()
+	    timeRemaining = (end / (start - end)) / 60
+	    time.sleep(10)
+
+    def stop(self):
+        self.stopped = True
+
+def main():
+    load_file_into_queue()
+    try:
+	for i in range(4):
+	    thread = MyThread(queue, printQueue)
+	    t.append(thread)
+	thread = timerThread(queue,printQueue)
+	t.append(thread)
+	print("Minutes\tQueue\tStatus\tPassphrase")	
+	while not queue.empty():
+	    sys.stdout.write("\r                                                     ")
+	    sys.stdout.write("\r" + str(timeRemaining) + "\t"+ printQueue.get())
+	    sys.stdout.flush()
+	    if found == True:
+		raise KeyboardInterrupt
+    except KeyboardInterrupt:
+	print("closing threads... ")
+	for thread in t:
+	    if thread.isAlive():
+		thread.stop()
+	print("Exiting Program.")
+	return
+    except:
+	print("Fuck, a thread unable to start")
+	print(sys.exc_info()[0])
+    if(queue.empty()):
+	with open("passphrase" + str(fileCount), 'r') as original: data = original.read()
+	with open('passphrase' + str(fileCount), 'w') as modified: modified.write("FILECOMPLETED\n" + data)
+
+
+def load_file_into_queue():
+    nextFile = False
+    while os.path.isfile('passphrase' + str(fileCount)):
+	f = open("passphrase" + str(fileCount), 'r')
+	for line in f:
+	    check = line.rstrip()
+	    if not "FILECOMPLETED" in check:
+		queue.put(line.rstrip())
+	    else:
+		nextFile = True
+		break
+	f.close()
+	if nextFile == True:
+	    i +=1
+	    nextFile = False
+	else:
+	    return
+    print("ERROR: No more unchecked files")
+    os._exit(1)
+
 
 if __name__ == '__main__':
     if not options.update:
-        print("Updating program...")
-        args = shlex.split("curl -o source.py https://raw.githubusercontent.com/alfanhui/dmgCracker/master/source.py")
-        proc = Popen(args, stdout=PIPE, stderr=PIPE)
-        print("program updated.")
-        os._exit(1)
+	print("Updating program...")
+	args = shlex.split("curl -o source.py https://raw.githubusercontent.com/alfanhui/dmgCracker/master/source.py")
+	proc = Popen(args, stdout=PIPE, stderr=PIPE)
+	print("program updated.")
+	os._exit(1)
     if os.path.isfile("PASSWORD IN HERE.txt"):
         print("Did you know the password may have been found?")
         raw_input("please check your folder!")
@@ -118,10 +142,7 @@ if __name__ == '__main__':
         os._exit(1)
     found = False
     start = time.time()
-    main(False)
+    main()
     end = time.time()
     print("Time taken to complete: ", end - start , " seconds..")
     os._exit(1)
-
-
-
